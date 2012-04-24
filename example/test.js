9bf1401a59203a7b6801f881df253565ebc4996e
Tim Caswell <tim@creationix.com>
2012-04-24 12:18:10 -0500

    Put both vfs-ssh and vfs-child in example

--- example/test.js
+++ example/test.js
@@ -1,15 +1,16 @@
-var port = 5000;
-var root = "http://creationix.com:" + port + "/rest/";
-
-var vfs = require('vfs-ssh')({
-  host: "creationix.com",
-  user: "tim",
-  root: "/home/tim/creationix.com/",
-  httpRoot: root,
-});
 
 require('http').createServer(require('stack')(
-  require('vfs-http-adapter')("/rest/", vfs)
-)).listen(port);
+    require('vfs-http-adapter')("/child/", require('vfs-child')({
+        root: "/home/tim/vfs/",
+        httpRoot: "http://localhost:8080/child/",
+    })),
+      require('vfs-http-adapter')("/ssh/", require('vfs-ssh')({
+        host: "tim@creationix.com",
+        nodePath: "/home/tim/nvm/v0.6.15/bin/node",
+        root: "/home/tim/creationix.com/",
+        httpRoot: "http://localhost:8080/ssh/",
+    }))
+)).listen(8080);
 
-console.log("RESTful interface at " + root);
+console.log("HTTP server using creationix.com ssh tunnel at http://localhost:8080/ssh/");
+console.log("HTTP server using vfs child process at http://localhost:8080/child/");
