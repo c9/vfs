78df984f79d2f0a0db7c3bc6907a1690d53854d2
Tim Caswell <tim@creationix.com>
2012-04-25 10:10:04 -0500

    Add local example

--- example/test.js
+++ example/test.js
@@ -1,5 +1,11 @@
 
 require('http').createServer(require('stack')(
+    require('vfs-http-adapter')("/local/", require('vfs-local')({
+        host: "localhost",
+        nodePath: process.execPath,
+        root: "/home/tim/Danger/",
+        httpRoot: "http://localhost:8080/local/",
+    })),
     require('vfs-http-adapter')("/child/", require('vfs-child')({
         root: "/home/tim/vfs/",
         httpRoot: "http://localhost:8080/child/",
@@ -12,5 +18,6 @@ require('http').createServer(require('stack')(
     }))
 )).listen(8080);
 
+console.log("HTTP server using local vfs at http://localhost:8080/local/");
 console.log("HTTP server using creationix.com ssh tunnel at http://localhost:8080/ssh/");
 console.log("HTTP server using vfs child process at http://localhost:8080/child/");
