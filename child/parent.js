720000dd05e8b0f7d48bcda72a56c134891562c3
Tim Caswell <tim@creationix.com>
2012-04-26 17:46:35 -0500

    Minor cleanup in vfs-child

--- child/parent.js
+++ child/parent.js
@@ -1,12 +1,15 @@
 var consumer = require('vfs-socket/consumer');
 var spawn = require('child_process').spawn;
 var Pipe;
+
 // Simple vfs that uses vfs-socket over a parent-child process relationship
 module.exports = function setup(fsOptions) {
     if (!Pipe) Pipe = process.binding('pipe_wrap').Pipe;
     var options = { customFds: [-1, 1, 2], stdinStream: new Pipe(true) };
     var args = [require.resolve('./child.js'), JSON.stringify(fsOptions)];
-    var child = spawn(process.execPath, args, options);
+    var executablePath = process.execPath;
+
+    var child = spawn(executablePath, args, options);
     child.stdin.resume();
     child.stdin.readable = true;
     return consumer({input: child.stdin});
