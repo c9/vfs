e87b570c58d2e2e52c76f4bed58e4d7e258da0db
Tim Caswell <tim@creationix.com>
2012-05-09 14:48:24 -0500

    Make ssh example be just ssh, using async API

--- example/ssh.js
+++ example/ssh.js
@@ -1,23 +1,12 @@
-
-require('http').createServer(require('stack')(
-    require('vfs-http-adapter')("/local/", require('vfs-local')({
-        host: "localhost",
-        nodePath: process.execPath,
-        root: "/home/tim/Danger/",
-        httpRoot: "http://localhost:8080/local/",
-    })),
-    require('vfs-http-adapter')("/child/", require('vfs-child')({
-        root: "/home/tim/vfs/",
-        httpRoot: "http://localhost:8080/child/",
-    })),
-      require('vfs-http-adapter')("/ssh/", require('vfs-ssh')({
-        host: "tim@creationix.com",
-        nodePath: "/home/tim/nvm/v0.6.15/bin/node",
-        root: "/home/tim/creationix.com/",
-        httpRoot: "http://localhost:8080/ssh/",
-    }))
-)).listen(8080);
-
-console.log("HTTP server using local vfs at http://localhost:8080/local/");
-console.log("HTTP server using creationix.com ssh tunnel at http://localhost:8080/ssh/");
-console.log("HTTP server using vfs child process at http://localhost:8080/child/");
+require('vfs-ssh')({
+    host: "tim@creationix.com",
+    nodePath: "/home/tim/nvm/v0.6.17/bin/node",
+    root: "/home/tim/creationix.com/",
+}, function (err, ssh) {
+    if (err) throw err;
+    require('http').createServer(require('stack')(
+        require('vfs-http-adapter')("/ssh/", ssh)
+    )).listen(8080, function () {
+        console.log("ssh filesystem listening at http://localhost:8080/ssh/");
+    });
+});
