d188947b6ad63b8ab684be6d9c3587c4df673480
Tim Caswell <tim@creationix.com>
2012-07-09 11:32:28 -0500

    vfs-local: Add basic event interface

---
+++ example/test-events.js
@@ -0,0 +1,30 @@
+// Test real version
+// var vfs = require('vfs-local')({
+//   root: __dirname + "/"
+// });
+// test(vfs);
+
+// Test socket proxied version
+var Parent = require('vfs-child').Parent;
+var parent = new Parent({root: __dirname + "/"});
+parent.connect(function (err, vfs) {
+  if (err) throw err;
+  test(vfs);
+});
+
+function test(vfs) {
+  vfs.on("MONKEY", onMonkey, function () {
+    console.log("MONKEY is registered");
+    vfs.emit("MONKEY", "EATS BANANNAS", function () {
+      console.log("MONKEY is emitted");
+      vfs.off("MONKEY", onMonkey, function () {
+        console.log("MONKEY is no longer listening");
+      });
+    });
+  });
+}
+
+function onMonkey(message) {
+  console.log("MONKEY %s", message);
+}
+
