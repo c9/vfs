8c80cf54b2117bb9ae2699619b030d3ecaba55d2
Tim Caswell <tim@creationix.com>
2012-08-10 22:02:53 +0000

    Add more vfs-local tests

--- test/test-local.js
+++ test/test-local.js
@@ -12,7 +12,7 @@ describe('vfs-local', function () {
     checkSymlinks: true
   }));
 
-  describe('#resolve()', function () {
+  describe('vfs.resolve()', function () {
 
     it('should prepend root when resolving virtual paths', function (done) {
       var vpath = "/dir/stuff.json";
@@ -38,7 +38,7 @@ describe('vfs-local', function () {
     });
   });
 
-  describe('#stat()', function () {
+  describe('vfs.stat()', function () {
     it('should return stat info for the text file', function (done) {
       vfs.stat("/file.txt", {}, function (err, stat) {
         if (err) return done(err);
@@ -50,25 +50,60 @@ describe('vfs-local', function () {
     });
     it("should error with ENOENT when the file doesn't exist", function (done) {
       vfs.stat("/badfile.json", {}, function (err, stat) {
-        expect(err).property("code").equals("ENOENT");
+        expect(err).property("code").equal("ENOENT");
         done();
       });
     });
   });
-});
 
-//  readdir: function () {
-//    vfs.readdir("/", {}, function (err, meta) {
-//      if (err) throw err;
-//      console.log(meta);
-//      meta.stream.on("data", function (stat) {
-//        console.log(stat);
-//      });
-//      meta.stream.on("end", function () {
-//        console.log("DONE");
-//        next();
-//      });
-//    });
-//  }
-//};
-//
+  describe('vfs.readfile()', function () {
+    it("should read the text file", function (done) {
+      vfs.readfile("/file.txt", {}, function (err, meta) {
+        if (err) return done(err);
+        expect(meta).property("mime").equals("text/plain");
+        expect(meta).property("size").equals(23);
+        expect(meta).property("etag");
+        expect(meta).property("stream").property("readable");
+        var stream = meta.stream;
+        var chunks = [];
+        var length = 0;
+        stream.on("data", function (chunk) {
+          chunks.push(chunk);
+          length += chunk.length;
+        });
+        stream.on("end", function () {
+          expect(length).equal(23);
+          var body = chunks.join("");
+          expect(body).equal("This is a simple file!\n");
+          done();
+        });
+      });
+    });
+    it("should error with ENOENT on missing files", function (done) {
+      vfs.readfile("/badfile.json", {}, function (err, stat) {
+        expect(err).property("code").equal("ENOENT");
+        done();
+      });
+    });
+  });
+
+  describe('vfs.readdir()', function () {
+    it("should read the directory", function (done) {
+      vfs.readdir("/", {}, function (err, meta) {
+        if (err) return done(err);
+        expect(meta).property("etag");
+        expect(meta).property("stream").property("readable");
+        var stream = meta.stream;
+        var parts = [];
+        stream.on("data", function (part) {
+          parts.push(part);
+        });
+        stream.on("end", function () {
+          expect(parts).length(5);
+          done();
+        });
+      });
+    });
+  });
+
+});
