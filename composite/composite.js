bdfecd2cf4a766cf088910817a794345977b44c9
Luke Chavers <luke@c2cschools.com>
2014-07-31 04:20:27 -0500

    Update composite.js
    Added 'mount.transformReturnPaths' option

--- composite/composite.js
+++ composite/composite.js
@@ -1,3 +1,4 @@
+var Stream = require('stream').Stream;
 
 module.exports = function () {
     var mounts = [];
@@ -54,6 +55,31 @@ module.exports = function () {
                         }
                         return callback(err);
                     }
+
+
+					// Path transformation logic
+					if( mount.transformReturnPaths === true ) {
+
+						switch( name ) {
+
+							case "readdir":
+								if( meta.stream !== undefined && meta.stream !== null ) {
+									meta.stream = getPathTransformerProxy( name, mount, meta );
+								}
+								break;
+
+							case "stat":
+								if( meta.path !== undefined ) {
+									meta.path = transformPath( mount, meta.path );
+								}
+								break;
+
+						}
+
+					}
+					// End path transformation logic
+
+
                     return callback(null, meta);
                 });
             }());
@@ -80,6 +106,20 @@ module.exports = function () {
     function mount(mount) {
         if (!mount.root) throw new Error("Mount must contain a root");
         if (!mount.vfs) throw new Error("Mount must contain a vfs instance");
+
+		// options.transformReturnPaths
+		// when TRUE paths will be transformed into
+		// an appropriate composite path before being
+		// returned to the implementor.
+		if( mount.transformReturnPaths === undefined ||
+			mount.transformReturnPaths === false ||
+			mount.transformReturnPaths === null ) {
+			mount.transformReturnPaths = false;
+		} else {
+			mount.transformReturnPaths = true;
+		}
+		// end options.transformReturnPaths
+
         var root = mount.root;
         if (root.substr(root.length - 1) !== "/") {
             throw new Error("Root must end in /");
@@ -98,3 +138,74 @@ module.exports = function () {
     }
 };
 
+/**
+ * Creates a proxy stream that transforms incoming
+ * file and directory paths to their appropriate
+ * composite path.
+ *
+ * @param {string} name
+ * @param {object} mount
+ * @param {object} meta
+ * @param {Stream} meta.stream
+ * @returns {Stream}
+ */
+function getPathTransformerProxy( name, mount, meta ) {
+
+	// Init
+	var originalStream = meta.stream;
+
+	// Setup the proxy/transform stream
+	var proxyStream = new Stream();
+	proxyStream.readable = true;
+
+	// Transform path
+	originalStream.on("data", function( part ) {
+		if( part.path !== undefined ) {
+			part.path = transformPath( mount, part.path );
+		}
+		proxyStream.emit("data",part);
+	});
+
+	// Forward this event verbatim
+	originalStream.on("end", function() {
+		proxyStream.emit("end");
+	});
+
+	// Forward these methods verbatim
+	proxyStream.pause = function () {
+		originalStream.pause;
+	};
+	proxyStream.resume = function () {
+		originalStream.resume;
+	};
+
+	// Return the new stream
+	return proxyStream;
+
+}
+
+/**
+ * Transforms a single path from the original
+ * into a proper composite path.
+ *
+ * @param {object} mount
+ * @param {string} path
+ * @returns {string}
+ */
+function transformPath( mount, path ) {
+
+	// Prefix is optional, so we check for it
+	if( mount.prefix !== undefined ) {
+		var pfLen = mount.prefix.length;
+		if( path.substr( 0, pfLen ) == mount.prefix ) {
+			path = path.substr( pfLen );
+		}
+	}
+
+	// All mounts should have a root
+	path = mount.root + path;
+
+	// Done
+	return path;
+
+}
